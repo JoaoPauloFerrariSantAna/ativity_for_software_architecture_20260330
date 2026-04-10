b53e8525c1c7bda824e346b6ab17bda49cbae7ac
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-10 15:32:15 -0300

    feat: added new value object

---
+++ WorkFromSchool_20260408/BillingContext/Application/Domain/ValueObjects/NameObject.cs
@@ -0,0 +1,17 @@
+﻿using WorkFromSchool_20260408.BillingContext.Application.Domain.Interfaces;
+
+namespace WorkFromSchool_20260408.BillingContext.Application.Domain.ValueObjects;
+
+public class NameObject
+{
+    private readonly IClienteRepository _clienteRepository;
+
+    public string Name { get; private set; }
+
+    public NameObject(string name)
+    {
+        if (String.IsNullOrEmpty(name)) throw new Exception("Name field is empty");
+
+        this.Name = name;
+    }
+}
