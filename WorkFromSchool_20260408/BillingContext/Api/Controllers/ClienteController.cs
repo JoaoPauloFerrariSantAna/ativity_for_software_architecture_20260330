516500fe84e7232c15613540dd118da46abd47df
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-10 15:37:56 -0300

    feat: added new methods to the api

--- WorkFromSchool_20260408/BillingContext/Api/Controllers/ClienteController.cs
+++ WorkFromSchool_20260408/BillingContext/Api/Controllers/ClienteController.cs
@@ -3,6 +3,7 @@ using WorkFromSchool_20260408.BillingContext.Application.Domain.Interfaces;
 using WorkFromSchool_20260408.BillingContext.Application.Domain.UseCases;
 using WorkFromSchool_20260408.BillingContext.Application.Domain.Enums;
 using WorkFromSchool_20260408.BillingContext.Application.Domain.ValueObjects;
+using WorkFromSchool_20260408.BillingContext.Application.Domain.Entities;
 
 namespace WorkFromSchool_20260408.BillingContext.Api.Controllers;
 
@@ -17,6 +18,41 @@ public class ClienteController : ControllerBase
         _clienteRepository = clienteRepository;
     }
 
+    [HttpGet("/getAll")]
+    public IActionResult GetAll()
+    {
+        return Ok(_clienteRepository.All());
+    }
+
+    [HttpGet("getById:id")]
+    public IActionResult GetById(int id)
+    {
+        try
+        {
+            return Ok(_clienteRepository.GetCliente(id));
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpPost("postCliente")]
+    public IActionResult PostCliente(string name, decimal amount)
+    {
+        try
+        {
+            Cliente cliente = new Cliente((new NameObject(name)).Name, (new AmountObject(amount)).Amount);
+            _clienteRepository.Post(cliente);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+
+        return Created();
+    }
+
     [HttpPost("makeDeposit")]
     public IActionResult Deposit(MetodoPagamento metodo, decimal amount, int id)
     {
@@ -24,11 +60,33 @@ public class ClienteController : ControllerBase
         {
             new MetodoPagamentoObject(metodo);
             MakeDepositCase mdc = new MakeDepositCase(_clienteRepository);
-            mdc.Deposit(id, new AmountObject(amount));
+            mdc.Deposit(id, (new AmountObject(amount)).Amount);
         }
         catch (Exception ex)
         {
             return BadRequest(ex.Message);
+        }
+
+        return Ok();
+    }
+
+    [HttpPatch("update")]
+    public IActionResult Update([FromBody] Cliente cliente)
+    {
+        _clienteRepository.Update(cliente);
+        return Ok();
+    }
+
+    [HttpDelete("deleteById:id")]
+    public IActionResult DeleteById(int id)
+    {
+        try
+        {
+            _clienteRepository.Delete(id);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
         }
 
         return Ok();
