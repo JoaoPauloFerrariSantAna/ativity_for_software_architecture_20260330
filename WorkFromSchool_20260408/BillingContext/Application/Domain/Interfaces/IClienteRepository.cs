c76e6a003c7f120f4d5827c71475bc4ef5e02a0a
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-10 15:56:46 -0300

    ref: added more methods to the interface and its implementation

--- WorkFromSchool_20260408/BillingContext/Application/Domain/Interfaces/IClienteRepository.cs
+++ WorkFromSchool_20260408/BillingContext/Application/Domain/Interfaces/IClienteRepository.cs
@@ -5,5 +5,10 @@ namespace WorkFromSchool_20260408.BillingContext.Application.Domain.Interfaces;
 public interface IClienteRepository
 {
     public Cliente GetCliente(int id);
+    public List<Cliente> All();
+    public void Post(Cliente cliente);
     public void Update(Cliente cliente);
+    public void Delete(int id);
+    public void Deposit(Cliente cliente, decimal amount);
+    public bool IsInDatabase(int id);
 }
--- WorkFromSchool_20260408/BillingContext/Infrastructure/Repositories/ClienteRepository.cs
+++ WorkFromSchool_20260408/BillingContext/Infrastructure/Repositories/ClienteRepository.cs
@@ -2,27 +2,59 @@
 using WorkFromSchool_20260408.BillingContext.Application.Domain.Interfaces;
 
 namespace WorkFromSchool_20260408.BillingContext.Infrastructure.Repositories;
+
+using DatabaseEntities = List<Cliente>;
+
 public class ClienteRepository : IClienteRepository
 {
-    private static List<Cliente> clienteList = new List<Cliente>();
+    private static DatabaseEntities _clienteList = new List<Cliente>();
 
     public void Deposit(Cliente cliente, decimal amount)
     {
         throw new NotImplementedException();
     }
 
+    public DatabaseEntities All()
+    {
+        return _clienteList;
+    }
+
+    public void Post(Cliente cliente)
+    {
+        if (cliente == null) throw new Exception("Client data is empty");
+
+        if (IsInDatabase(cliente.Id)) throw new Exception("It is in database already");
+
+        // TODO: Add more fields tto validate
+
+        _clienteList.Add(cliente);
+    }
+    public void Update(Cliente cliente)
+    {
+        // it is ok to just place this:
+        // ClienteRepository::GetCliente returns an exception if the id is not found in DB
+        // witch will go down in the callstack
+        Cliente c = GetCliente(cliente.Id);
+        c = cliente;
+    }
+
+    public void Delete(int id)
+    {
+        // it is ok to just place this:
+        // ClienteRepository::GetCliente returns an exception if the id is not found in DB
+        // witch will go down in the callstack
+        _clienteList.Remove(GetCliente(id));
+    }
+
     public Cliente GetCliente(int id)
     {
-        throw new NotImplementedException();
+        if(!IsInDatabase(id)) throw new Exception("Cliente is not in database");
+        return _clienteList.Find(cl => id == cl.Id);
     }
 
     public bool IsInDatabase(int id)
     {
-        throw new NotImplementedException();
-    }
-
-    public void Update(Cliente cliente)
-    {
-        throw new NotImplementedException();
+        foreach (Cliente c in _clienteList) if (id == c.Id) return true;
+        return false;
     }
 }
