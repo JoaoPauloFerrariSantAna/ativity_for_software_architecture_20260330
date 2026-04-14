9f2b9c7403595691a8326bd1e7f4fd3c14a96665
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-14 20:17:14 -0300

    feat: updated IClienteRepository

--- WorkFromSchool_20260408/BillingContext/Application/Domain/Interfaces/IClienteRepository.cs
+++ WorkFromSchool_20260408/BillingContext/Application/Domain/Interfaces/IClienteRepository.cs
@@ -1,14 +1,16 @@
 ﻿using WorkFromSchool_20260408.BillingContext.Application.Domain.Entities;
+using WorkFromSchool_20260408.BillingContext.Infrastructure.Repositories;
 
 namespace WorkFromSchool_20260408.BillingContext.Application.Domain.Interfaces;
-
 public interface IClienteRepository
 {
-    public Cliente GetCliente(int id);
+    public Cliente GetCliente(Guid id);
+    public Carteira GetCarteira(Guid carteiraId);
     public List<Cliente> All();
     public void Post(Cliente cliente);
     public void Update(Cliente cliente);
-    public void Delete(int id);
+    public void Delete(Guid id);
     public void Deposit(Cliente cliente, decimal amount);
-    public bool IsInDatabase(int id);
+    public bool IsInDatabase(Guid id);
+    public bool HasCarteira(Guid carteiraId);
 }
