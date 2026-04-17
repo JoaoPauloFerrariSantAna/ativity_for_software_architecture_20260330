3a8abd0a1d581e786db26e49c58cbb5d29794cb3
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-04-17 14:31:52 -0300

    feat: added a new use case made from the group

---
+++ WorkFromSchool_20260408/BillingContext/Application/Domain/ValueObjects/NumeroCartaoObject.cs
@@ -0,0 +1,14 @@
+﻿namespace Billing.Domain.ValueObjects;
+
+public record NumeroCartao
+{
+    public string Numero { get; }
+
+    public NumeroCartao(string numero)
+    {
+        if (string.IsNullOrWhiteSpace(numero) || numero.Length < 13)
+            throw new ArgumentException("Número do cartão inválido.");
+
+        Numero = numero;
+    }
+}
